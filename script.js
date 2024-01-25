0a67da2e0b6f6faf0eae5ed35cfc9f9743b1fe6c
Avi194 <84580776+Avi194@users.noreply.github.com>
2024-01-25 12:11:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,86 @@
+const btn = document.querySelector("#start");
+var reset = document.querySelector("#reset");
+const guess = document.querySelector("#numInput");
+var hint = document.getElementById("hint");
+var bar = document.querySelectorAll("#hearts");
+var hearts = 5;
+reset.style.visibility ='hidden';
+btn.style.visibility ='visible';
+    var random =  Math.floor(Math.random()*100) + 1;
+
+console.log("Default " +random);
+
+function playSound(audioName,loop){
+    let audio = new Audio(audioName);
+    audio.loop = loop;
+    audio.play();
+}
+function retry(){
+    hearts = 5;
+    reset.style.visibility = 'hidden';
+    btn.style.visibility='visible';
+    hint.innerHTML = "";
+    guess.value = "";
+    random =  Math.floor(Math.random()*100) + 1;
+    console.log("random "+random);
+    for(var i = 0;i < bar.length;i++)
+    {
+    bar[i].style.visibility = 'visible';
+    }
+}
+btn.addEventListener("click",()=>{
+    if(guess.value > random){
+       hint.innerHTML = "Too High Try Again";
+       playSound('decrease.mp3',false);
+       hearts = hearts-1;
+       console.log("hearts "+hearts);
+       guess.value = "";
+       if (hearts < 5){
+        bar[hearts].style.visibility = 'hidden'
+            }
+            if(hearts == 0){
+                playSound('death.mp3',false);
+                hint.innerHTML = "You Loose The Number was "+random;
+                reset.style.visibility ='visible';
+                btn.style.visibility = 'hidden'
+            }
+    }
+    else if(guess.value < random){
+      hint.innerHTML = "Too Low Try Again ";
+      playSound('decrease.mp3',false);
+      hearts = hearts-1;
+      console.log("hearts "+hearts);
+      guess.value = "";
+      if (hearts < 5){
+        bar[hearts].style.visibility = 'hidden'
+            }
+            if(hearts == 0){
+                playSound('death.mp3',false);
+                hint.innerHTML = "You Loose The Number was "+random;
+                reset.style.visibility ='visible';
+                btn.style.visibility = 'hidden'
+            }
+    }
+    else if(guess.value == random){
+    hint.innerHTML = "WoW you are correct ";
+    hearts = 5 ;
+    guess.value = "";
+
+    for(var i = 0;i < bar.length;i++)
+    {
+    bar[i].style.visibility = 'visible';
+    }
+    console.log("hearts "+hearts);
+    random = 0;
+    random =  Math.floor(Math.random()*100) + 1;
+    console.log("random "+random);
+    reset.style.visibility = 'hidden';
+    btn.style.visibility = 'visible';
+    playSound('win.mp3',false);
+    setTimeout(()=>{
+        hint.innerHTML = "";
+    },10000);                    
+
+}
+
+})
